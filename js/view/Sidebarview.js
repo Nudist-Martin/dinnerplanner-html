6d67ca620ab366eae952395eb449510f0978359b
Martin Lindberg <marlin5@kth.se>
2019-01-30 22:59:11 +0100

    Knack 2 Bootstrap Babyyyyy

--- js/view/Sidebarview.js
+++ js/view/Sidebarview.js
@@ -1,3 +1,29 @@
+<<<<<<< HEAD
+class People {
+  constructor (container, model){
+    var guests = container.find("#guests");
+    guests.value = 5;
+  }
+}
+class Price {
+  constructor (container, model){
+  //  alert(model.getTotalMenuPrice());
+    var price = container.find("#TotalPrice");
+    price.html(model.getTotalMenuPrice());
+  }
+}
+var elMainSelect = document.getElementById('mainSelect');
+
+function selectOptionsCreate() {
+  var frag = document.createDocumentFragment(),
+    elOption;
+  for (var i=12; i<101; ++i) {
+    elOption = frag.appendChild(document.createElement('option'));
+    elOption.text = i;
+  }
+  elMainSelect.appendChild(frag);
+}
+=======
 class People {
   constructor (container, model){
     var guests = container.find("#numberOfGuests");
@@ -11,3 +37,4 @@ class Price {
     price.html(model.getTotalMenuPrice());
   }
 }
+>>>>>>> e6c2c21b0c79e212fe7faca770e7c1356bcba237
